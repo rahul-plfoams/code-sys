d9f6fa96d7387c9580dbbb2e5ea65238e97fa00e
rahul-plfoams <rahulrai@plfoams.in>
2019-11-29 11:23:45 +0530

    remove from js practice

--- application/controllers/Products.php
+++
@@ -1,7 +0,0 @@
-<?php 
-defined("BASEPATH") or exit("No direct script access allowed");
-class Products extends CI_controller{
-    function index(){
-        echo "this is products page";
-    }
-}
